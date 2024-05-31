00c39dc9d9f906a723700a222582082c2a5f7f7c
nikolinaivankovic96 <113460177+nikolinaivankovic96@users.noreply.github.com>
2024-05-31 02:24:40 +0200

    Create aci.tf

---
+++ modules/environment/aci.tf
@@ -0,0 +1,50 @@
+resource "azurerm_container_group" "main" {
+  name                = "demo-aci-${var.environment}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  ip_address_type     = "Public"
+  dns_name_label      = "demo-${var.environment}"
+  os_type             = "Linux"
+
+  image_registry_credential {
+        server   = azurerm_container_registry.main.login_server
+        username = azurerm_container_registry.main.admin_username
+        password = azurerm_container_registry.main.admin_password
+    }
+
+  container {
+    name   = "website"
+    image  = "${azurerm_container_registry.main.login_server}/${var.environment}:latest"
+    cpu    = "1.0"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  container {
+    name   = "nginx-ssl"
+    image  = "mcr.microsoft.com/oss/nginx/nginx:1.15.5-alpine"
+    cpu    = "1.0"
+    memory = "1.5"
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+
+    volume {
+      name      = "nginx-config"
+      mount_path = "/etc/nginx"
+      read_only   = false
+
+      secret = {
+      "ssl.crt"    = data.azurerm_key_vault_secret.ssl_crt.value
+      "ssl.key"    = data.azurerm_key_vault_secret.ssl_key.value
+      "nginx.conf" = data.azurerm_key_vault_secret.nginx_conf.value
+        }
+    }
+  }
+}
